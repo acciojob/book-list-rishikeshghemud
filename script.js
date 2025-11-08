0ec4572eb4ec8bc9f24f3bd8dd040ea87059341e
Rishikesh <146565427+rishikeshghemud@users.noreply.github.com>
2025-11-09 00:18:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 //your JS code here. If required.
+
+const submitBtn = document.querySelector('#submit');
+const tbody = document.querySelector('#book-list')
+const form = document.querySelector('form')
+
+form.addEventListener('submit', (e) => {
+	e.preventDefault();
+	const title = document.querySelector('#title');
+	const author = document.querySelector('#author');
+	const isbn = document.querySelector('#isbn');
+
+	const row = document.createElement('tr');
+	row.innerHTML = `
+		<tr>
+			<td>${title.value}</td>
+			<td>${author.value}</td>
+			<td>${isbn.value}</td>
+			<td><button class="delete">x</button></td>
+		</tr>
+	`;
+
+	tbody.appendChild(row);
+	
+	form.reset();
+});
+
+tbody.addEventListener('click', (e)=> {
+	if(e.target.classList.contains('delete')) {
+		e.target.closest('tr').remove();
+	}
+});
